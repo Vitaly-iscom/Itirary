c7bd82c84eeafe7f77983164e91d458b191778d4
Vitaly-iscom <vetall_ekt@mail.ru>
2018-12-25 11:01:43 +0500

    Create delete and edit function

--- ItiraryApp/ItiraryApp/Resources/Extentions/UIButtonExtension.swift
+++ ItiraryApp/ItiraryApp/Resources/Extentions/UIButtonExtension.swift
@@ -13,7 +13,7 @@ extension UIButton {
     func createFloatingActionButton() {
         backgroundColor = Theme.tint
         layer.cornerRadius = frame.height / 2
-        tintColor = Theme.accent
+        tintColor = Theme.backgroundColor
         layer.shadowOpacity = 0.25
         layer.shadowRadius = 5
         layer.shadowOffset = CGSize(width: 0, height: 10)
